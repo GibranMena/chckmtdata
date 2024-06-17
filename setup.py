c0f0422babaef31b5c015434b9b26ae7fc188f6b
GibranMena <gibran.mena@gmail.com>
2024-06-17 13:02:55 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Checkmate_data_structure',
+    version='0.1.0',
+    author='Gibran Mena',
+    description='a data structure for Checkmate',
+    python_requires='>=3',
+    license='GNU General Public License v3',
+    packages=find_packages(),
+)
